9225100bc6a1f0977a97cbe71d9dc5f0220b0333
Muhammad-Javad <44828583+Muhammad-Javad@users.noreply.github.com>
2020-10-02 23:36:50 +0330

    convert from groovy to kotlin in gradle's files .

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+
 buildscript {
-    ext.kotlin_version = "1.4.0"
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:4.0.0"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -21,6 +22,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean").configure {
+    delete("build")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
-include ':app'
+include(":app")
 rootProject.name = "Kotlin Build script practice"
